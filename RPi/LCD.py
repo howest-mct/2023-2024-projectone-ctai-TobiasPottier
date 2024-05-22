f28a4bcf077ff43538e3ca4d4c2de749535d94f2
TobiasPottier <tobias.pottier@student.howest.be>
2024-05-22 14:55:10 +0200

    FEAT: Implemented LCD that displays the incoming messages and the MAC address

---
+++ RPi/LCD.py
@@ -0,0 +1,67 @@
+from smbus import SMBus
+import time
+
+
+class LCD:
+    I2C_ADDR = 0x27  # I2C device address
+    LCD_CHR = 1       # Character mode
+    LCD_CMD = 0       # Command mode
+    LCD_LINE_1 = 0x80 # Instruction to go to beginning of line 1
+    LCD_LINE_2 = 0xC0 # Instruction to go to beginning of line 2
+    LCD_BACKLIGHT = 0x08  # Data bit value to turn backlight on
+    ENABLE = 0b00000100    # Enable bit value
+    E_DELAY = 0.0005       # Delay between pulses
+
+    def __init__(self):
+        self.bus = SMBus(1)
+        self.lcd_init()
+
+    def lcd_init(self):
+        self.send_byte(0x33, self.LCD_CMD) # initializing the LCD in 4-bit mode.
+        self.send_byte(0x32, self.LCD_CMD) # initializing the LCD in 4-bit mode.
+        self.send_byte(0x06, self.LCD_CMD) # entry mode, specifying how the cursor moves and whether the display shifts.
+        self.send_byte(0x0C, self.LCD_CMD) # turns off the cursor.
+        self.send_byte(0x28, self.LCD_CMD) # configures display lines and character font.
+        self.send_byte(0x01, self.LCD_CMD) # Clears the display.
+        time.sleep(0.05)
+
+    def send_byte(self, byte, mode):
+        bits_high = mode | (byte & 0xF0) | self.LCD_BACKLIGHT
+        bits_low = mode | ((byte & 0x0F) << 4) | self.LCD_BACKLIGHT
+        self.bus.write_byte(self.I2C_ADDR, bits_high)
+        self.send_byte_with_e_toggle(bits_high)
+        self.bus.write_byte(self.I2C_ADDR, bits_low)
+        self.send_byte_with_e_toggle(bits_low)
+
+    def send_byte_with_e_toggle(self, bits):
+        time.sleep(self.E_DELAY)
+        self.bus.write_byte(self.I2C_ADDR, (bits | self.ENABLE))
+        time.sleep(self.E_DELAY)
+        self.bus.write_byte(self.I2C_ADDR, (bits & ~self.ENABLE))
+        time.sleep(self.E_DELAY)
+
+    def send_instruction(self, instruction):
+        self.send_byte(instruction, self.LCD_CMD)
+
+    def send_character(self, char):
+        self.send_byte(ord(char), self.LCD_CHR)
+
+    def send_string(self, message, line=LCD_LINE_1):
+        self.send_instruction(line)
+        for char in message:
+            self.send_character(char)
+
+    def clear(self):
+        self.send_instruction(0x01)
+
+    def display_on(self):
+        self.send_instruction(0x0C)
+
+    def display_off(self):
+        self.send_instruction(0x08)
+
+    def cursor_on(self):
+        self.send_instruction(0x0E)
+
+    def cursor_off(self):
+        self.send_instruction(0x0C)
--- RPi/app.py
+++ RPi/app.py
@@ -1,5 +1,9 @@
 import threading
 import queue
+import LCD
+
+lcd = LCD.LCD()
+lcd.clear()
 
 # Bluez gatt uart service (SERVER)
 from bluetooth_uart_server.bluetooth_uart_server import ble_gatt_uart_loop
@@ -7,23 +11,33 @@ from bluetooth_uart_server.bluetooth_uart_server import ble_gatt_uart_loop
 # extend this code so the value received via Bluetooth gets printed on the LCD
 # (maybe together with you Bluetooth device name or Bluetooth MAC?)
 
+MAC_ADDRESS = "D8:3A:DD:D9:73:57"
+
 def main():
     i = 0
     rx_q = queue.Queue()
     tx_q = queue.Queue()
     device_name = "TPBias-pi-gatt-uart" # TODO: replace with your own (unique) device name
     threading.Thread(target=ble_gatt_uart_loop, args=(rx_q, tx_q, device_name), daemon=True).start()
-    while True:
-        try:
-            incoming = rx_q.get(timeout=1) # Wait for up to 1 second 
-            if incoming:
-                print("In main loop: {}".format(incoming))
-        except Exception as e:
-            pass # nothing in Q 
+    lcd.send_string(MAC_ADDRESS, lcd.LCD_LINE_2)
+    try:
+        while True:
+            try:
+                incoming = rx_q.get(timeout=1) # Wait for up to 1 second 
+                if incoming:
+                    message = "In main loop: {}".format(incoming)
+                    print(message)
+                    lcd.send_string(message, lcd.LCD_LINE_1)
+            except Exception as e:
+                pass # nothing in Q 
 
-        # if i%5 == 0: # Send some data every 5 iterations
-        #     tx_q.put("test{}".format(i))
-        # i += 1
+            # if i%5 == 0: # Send some data every 5 iterations
+            #     tx_q.put("test{}".format(i))
+            # i += 1
+    except:
+        pass
+    finally:
+        lcd.clear()
 if __name__ == '__main__':
     main()
 
