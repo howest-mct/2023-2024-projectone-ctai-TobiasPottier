a99462fdb7b7a87bc600ddbd9fed805256e81f50
Saibot72 <tobias.pottier@student.howest.be>
2024-05-28 19:40:53 +0200

    FEAT: New model training with new subset of 5000 training images and 2500 validation images for YOLO detection model

--- AI/testing/Detection/CreateSubsetDataset.py
+++ AI/testing/Detection/CreateSubsetDataset.py
@@ -16,17 +16,20 @@ def create_subset(input_image_dir, input_label_dir, output_image_dir, output_lab
     for image_file in subset_images:
         # Copy image file
         shutil.copy(os.path.join(input_image_dir, image_file), os.path.join(output_image_dir, image_file))
+        print(f'Coppied file: {image_file}')
         
         # Copy corresponding label file
         label_file = os.path.splitext(image_file)[0] + '.txt'
         shutil.copy(os.path.join(input_label_dir, label_file), os.path.join(output_label_dir, label_file))
+        print(f'Coppied file: {label_file}')
+
 
 # Define paths
 input_image_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/Dataset/images/train'
 input_label_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/Dataset/labels/train'
-output_image_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/SubSetDataset/images/train'
-output_label_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/SubSetDataset/labels/train'
-subset_size = 1000  # desired number of images
+output_image_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/SubSet5000Dataset/images/train'
+output_label_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/SubSet5000Dataset/labels/train'
+subset_size = 5000  # desired number of images
 
 # Create subset
 create_subset(input_image_dir, input_label_dir, output_image_dir, output_label_dir, subset_size)
@@ -34,8 +37,8 @@ create_subset(input_image_dir, input_label_dir, output_image_dir, output_label_d
 # Repeat for validation set
 input_image_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/Dataset/images/val'
 input_label_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/Dataset/labels/val'
-output_image_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/SubSetDataset/images/val'
-output_label_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/SubSetDataset/labels/val'
-subset_size = 500  # desired number of validation images
+output_image_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/SubSet5000Dataset/images/val'
+output_label_dir = 'C:/1-PC_M/1AI/ProjectOne/2ProjectOneGithub/DatasetDetection/SubSet5000Dataset/labels/val'
+subset_size = 2500  # desired number of validation images
 
 create_subset(input_image_dir, input_label_dir, output_image_dir, output_label_dir, subset_size)
